47454e2b9f4bee20d9c5b97a41b28f1e1644fe64
Roman lesyo <98737355+Romaleso261981@users.noreply.github.com>
2024-08-08 10:43:01 +0300

    create project

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import styles from "./Header.module.css";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.mainLogo}>Logo</div>
+      <nav>
+        <ul>
+          <li>
+            <Link href={"/"}>Main</Link>
+            <Link href={"/users"}>Users</Link>
+            <Link href={"/posts"}>Posts</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
